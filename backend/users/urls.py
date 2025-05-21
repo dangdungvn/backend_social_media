675b12312df37d64387a7d93959245dc48a6e917
dangdungvn <tungmj1605@gmail.com>
2025-05-21 13:35:33 +0700

    Fix user router registration in Django URLs for consistency and clarity

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,10 +3,10 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register(r"", views.UserViewSet)
-router.register(r"profiles", views.ProfileViewSet)
-router.register(r"friendships", views.FriendshipViewSet)
-router.register(r"follows", views.FollowViewSet)
+router.register(r"users", views.UserViewSet, basename="user")
+router.register(r"profiles", views.ProfileViewSet, basename="profile")
+router.register(r"friendships", views.FriendshipViewSet, basename="friendship")
+router.register(r"follows", views.FollowViewSet, basename="follow")
 
 urlpatterns = [
     path("", include(router.urls)),
